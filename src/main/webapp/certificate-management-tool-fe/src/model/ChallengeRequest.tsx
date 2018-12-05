0e75fdc25885530ea8317d25188199aad9a7855b
Taras <murzenkovtn@gmail.com>
2018-12-05 18:58:09 +0200

    fixed compilation error

--- src/main/webapp/certificate-management-tool-fe/src/model/ChallengeRequest.tsx
+++ src/main/webapp/certificate-management-tool-fe/src/model/ChallengeRequest.tsx
@@ -1,8 +1,8 @@
 export default class ChallengeRequest {
     userName: string;
-    domains: string;
+    domains: string[];
 
-    constructor(userName: string, domains: string) {
+    constructor(userName: string, domains: string[]) {
         this.userName = userName;
         this.domains = domains;
     }
